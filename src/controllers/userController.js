d8930867f345bf7d5b22203a169e1484d26be8a4
kenuy <guilhermesaavedranunes@gmail.com>
2024-12-11 23:07:32 -0400

    Cadastro pelo userController corrigido

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,17 @@ import { firestoreModel } from "../models/firestoreModel.js";
 import { authModel } from "../models/authModel.js";
 
 export const userController = {
+  cadastrarUsuario: async (email, password, nomeCompleto, tipoUsuario) => {
+    try {
+      const { user, message } = await authModel.cadastrar(email, password, nomeCompleto);
+      await userController.criarUsuario(user.uid, nomeCompleto, email, tipoUsuario);
+      return { message };
+    } catch (error) {
+      console.error("Erro ao cadastrar usuário:", error);
+      throw new Error(error.message || "Erro ao cadastrar usuário.");
+    }
+  },
+
   criarUsuario: async (uid, nomeCompleto, email, tipoUsuario) => {
     const usuario = {
       nome: nomeCompleto,
