0188723d75083fa0f0f5ab81f845c746e2b1e932
kenuy <guilhermesaavedranunes@gmail.com>
2024-12-15 15:09:42 -0400

    ultimo commit

--- src/views/professor/js/turmaRenderer.js
+++ src/views/professor/js/turmaRenderer.js
@@ -7,16 +7,17 @@ export const turmaRenderer = {
     turmaDiv.classList.add("turma-card");
 
     turmaDiv.innerHTML = `
-      <div class="turma-content">
-        <div class="turma-info">
-          <h3 class="turma-nome">${turma.nome}</h3>
-          <p class="turma-detalhe">Turma: ${turma.disciplina}</p>
-        </div>
-        <div class="turma-icone">
-          <i class="folder-icon">📁</i>
-        </div>
+    <div class="turma-content">
+      <div class="turma-info">
+      <h3 class="turma-nome">${turma.nome}</h3>
+      <p class="turma-detalhe">Turma: ${turma.disciplina}</p>
+    </div>
+      <div class="turma-icone">
+        <i class="folder-icon">📁</i>
       </div>
-      <a class="ver-tudo-btn" href="../professor/sala.html">Ver tudo</a>
+    </div>
+    <a class="ver-tudo-btn" href="../professor/sala.html">Ver tudo</a>
+    <button class="btn-excluir-turma">Excluir</button> <!-- Adicionado aqui -->
     `;
 
     container.appendChild(turmaDiv);
